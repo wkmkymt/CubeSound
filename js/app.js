e1a3a146c2e9c6cacc9c53e05f9a9765b0e97504
wkmkymt <wkmkymt@gmail.com>
2014-09-02 02:02:58 +0900

    Add rotation navigation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
-  var app = CubeSound3D("gameBoard", 3, 3, 6);
+  $(".rotation-nav .top    .button").hover(function() { $(".cube").toggleClass("rotate-top"); });
+  $(".rotation-nav .right  .button").hover(function() { $(".cube").toggleClass("rotate-right"); });
+  $(".rotation-nav .bottom .button").hover(function() { $(".cube").toggleClass("rotate-bottom"); });
+  $(".rotation-nav .left   .button").hover(function() { $(".cube").toggleClass("rotate-left"); });
+
+//  var app = CubeSound3D("gameBoard", 3, 3, 6);
 
 });
